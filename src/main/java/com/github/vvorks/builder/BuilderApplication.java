726e3a389b7fcaf1866c9a4aeb0afc657098115a
hiida <haruhikoiida@gmail.com>
2021-08-29 22:22:03 +0900

    add debug api

--- src/main/java/com/github/vvorks/builder/BuilderApplication.java
+++ src/main/java/com/github/vvorks/builder/BuilderApplication.java
@@ -1,13 +1,37 @@
 package com.github.vvorks.builder;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BuilderApplication {
 
+	private static final Class<BuilderApplication> THIS = BuilderApplication.class;
+
+    private static ConfigurableApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(BuilderApplication.class, args);
+		start(args);
+	}
+
+	public static void start(String[] args) {
+		context = SpringApplication.run(THIS, args);
+	}
+
+	public static void restart() {
+		ApplicationArguments args = context.getBean(ApplicationArguments.class);
+		Thread thread = new Thread(() -> {
+			context.close();
+			context = SpringApplication.run(THIS, args.getSourceArgs());
+		});
+		thread.setDaemon(false);
+		thread.start();
+	}
+
+	public static void stop() {
+		context.close();
 	}
 
 }
--- src/main/java/com/github/vvorks/builder/controller/ApiController.java
+++ src/main/java/com/github/vvorks/builder/controller/ApiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.vvorks.builder.BuilderApplication;
 import com.github.vvorks.builder.dto.Hoge;
 
 
@@ -17,6 +18,32 @@ public class ApiController {
 		hoge.setDoubleValue(1.0);
 		hoge.setStrValue(name);
 		return hoge;
+	}
+
+	/**
+	 * システム再起動
+	 *
+	 * TODO 危険なAPIなので何らかの制限処理が必要
+	 *
+	 * @return 応答文字列
+	 */
+	@GetMapping(path="/system/restart")
+	public String restart() {
+		BuilderApplication.restart();
+		return "done";
+	}
+
+	/**
+	 * システム停止
+	 *
+	 * TODO 危険なAPIなので何らかの制限処理が必要
+	 *
+	 * @return 応答文字列
+	 */
+	@GetMapping(path="/system/stop")
+	public String stop() {
+		BuilderApplication.stop();
+		return "done";
 	}
 
 }
